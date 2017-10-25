8fed47ec5cbb861c83498de6e6c73b95df902c46
hawkchen <hawkchen@potix.com>
2017-10-25 11:15:43 +0800

    add comments on AsyncRender

--- src/main/java/keikai/demo/zk/AsyncRender.java
+++ src/main/java/keikai/demo/zk/AsyncRender.java
@@ -5,9 +5,8 @@ import java.util.function.*;
 import org.zkoss.zk.ui.*;
 
 /**
- * In an asynchronous thread, to update ZK components requires activating a desktop first and deactivating after the update.
- * This class help you activate and deactivate, so you just need to pass a function that implements your rendering logic.
- * Please refer to https://www.zkoss.org/wiki/ZK_Developer%27s_Guide/Advanced_ZK/Long_Operations/Alternative_1:_Server_Push
+ * In an asynchronous thread, to update ZK components requires activating a desktop first and deactivating after accessing ZK Components. (Please refer to <a href="https://www.zkoss.org/wiki/ZK_Developer%27s_Guide/Advanced_ZK/Long_Operations/Alternative_1:_Server_Push">ZK Developer's_Guide/Advanced_ZK/Long_Operations/Alternative_1:_Server_Push</a>)
+ * This class helps you activate and deactivate, so you just need to pass a function that implements your rendering logic.
  * 
  */
 public class AsyncRender {
@@ -42,14 +41,20 @@ public class AsyncRender {
 
 	/**
 	 * return a Runnable that executes UI update logic between activation / de-activation of a desktop. (Template Method pattern)
-	 * it should be called in an ZK event listener.
-	 * @param update
+	 * it should be called in an ZK event listener since it gets a Desktop from the current Execution.
+	 * @param update a function that implements the rendering logic to access ZK components
 	 * @return a Runnable that executes UI update logic between activation / de-activation of a desktop
 	 */
 	static public Runnable getUpdateRunner(Runnable update){
 		return getUpdateRunner(getCurrentDesktop(), update);
 	}
 
+	/**
+	 * return a Runnable that executes UI update logic between activation / de-activation of a desktop. (Template Method pattern)
+	 * @param desktop ZK Desktop
+	 * @param update a function that implements the rendering logic to access ZK components
+	 * @return a Runnable that executes UI update logic between activation / de-activation of a desktop
+	 */
 	static public Runnable getUpdateRunner(Desktop desktop, Runnable update){
 		return () -> {
 			try {
