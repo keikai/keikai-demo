4dce387b2e0f883189c3db3362613976437a4177
hawkchen <hawkchen@potix.com>
2017-11-30 18:22:07 +0800

    update server IP

--- src/main/java/keikai/demo/Configuration.java
+++ src/main/java/keikai/demo/Configuration.java
@@ -4,5 +4,6 @@ package keikai.demo;
 
 
 public class Configuration {
-	public static final String KEIKAI_SERVER = "http://114.34.173.199:8888"; 
+	public static final String KEIKAI_SERVER = "http://220.134.32.242:8888";
+	public static final String INTERNAL_KEIKAI_SERVER = "http://10.1.3.201:7777";
 }
