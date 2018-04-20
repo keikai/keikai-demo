1c9c906032ac1926793a39e5e085cdfe69497351
hawkchen <hawkchen@potix.com>
2018-04-20 17:50:54 +0800

    * add demo server constant * update demo test file

--- src/main/java/keikai/demo/Configuration.java
+++ src/main/java/keikai/demo/Configuration.java
@@ -7,4 +7,5 @@ public class Configuration {
 	public static final String KEIKAI_SERVER = "http://220.134.32.242:8888";
 	public static final String INTERNAL_KEIKAI_SERVER = "http://10.1.3.201:7777";
 	public static final String DEMO_SERVER = "http://localhost:8888";
+	public static final String DEMO_BOOK_PATH = "/book/demo/";
 }
