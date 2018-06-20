9d09223ff9201d47429699b5856c112abb8096d4
hawkchen <hawkchen@potix.com>
2018-06-20 14:41:51 +0800

    change default keikai server to localhost

--- src/main/java/keikai/demo/Configuration.java
+++ src/main/java/keikai/demo/Configuration.java
@@ -4,6 +4,6 @@ package keikai.demo;
 
 
 public class Configuration {
-	public static final String KEIKAI_SERVER = "http://220.134.32.242:8888";
+	public static final String KEIKAI_SERVER = "http://localhost:8888";
 	public static final String INTERNAL_KEIKAI_SERVER = "http://10.1.3.201:7777";
 }
